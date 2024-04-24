7aac611690c3da734ddc32770b63eba4ff6dc57c
stepandel <stepandel@gmail.com>
2024-04-24 15:41:48 -0700

    parse a list of commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
-/bin/sqitch "$@"
+# Split the command input into an array by space
+IFS=' ' read -r -a cmd_array <<< "$1"
+
+# Execute sqitch with the array
+/bin/sqitch "${cmd_array[@]}" --chdir "$2" --target "$3"
